c6048d9805a1bf0a5bea4eb9c6ba8f6b61ca9444
viswa <viswanathanram1607@gmail.com>
2025-06-17 14:05:20 +0530

    adding environments file

---
+++ environments/qa/main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "../../modules/vpc"
+  vpc_cidr = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
+  subnet_az =  "${var.aws_region}a"  # Assuming the region is ap-southeast-1, this will be ap-southeast-1a
+  env = "dev"
+
+}
+
+module "ec2" {
+   source = "../../modules/ec2"
+   ami_id = "ami-0a7d80731ae1b2435"
+   instance_type = "t2.micro"
+   subnet_id = module.vpc.subnet_id
+   ec2_count = 2
+   env = "dev"
+
+}
