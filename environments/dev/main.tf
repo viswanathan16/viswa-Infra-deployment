ee4a4514723c83307babf76b52396c4142f0a22f
viswa <viswanathanram1607@gmail.com>
2025-06-17 22:09:48 +0530

    going to destroy the resources

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,4 @@
-module "vpc" {
+/*module "vpc" {
   source = "../../modules/vpc"
   vpc_cidr = "10.0.0.0/16"
   subnet_cidr = "10.0.1.0/24"
@@ -21,3 +21,4 @@ module "s3" {
   bucket_name = "viswa-infrabucket"
   env    = "dev"
 }
+*/
