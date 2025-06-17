f3d19a3c5de345d7f6fe4d3cb1a74108e3e9758e
viswa <viswanathanram1607@gmail.com>
2025-06-17 21:22:07 +0530

    made correction in bucket versioning block

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_acl" "example" {
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.my-s3
+  bucket = aws_s3_bucket.my-s3.id
   versioning_configuration {
     status = "Enabled"
   }
