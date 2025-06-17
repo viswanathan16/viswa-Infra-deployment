6019f90342b46d440d9010c89176985e8d5e004f
viswa <viswanathanram1607@gmail.com>
2025-06-17 23:01:13 +0530

    production branch

---
+++ environments/production/backend.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+
+backend "s3" {
+  bucket         = "viswa-terraform-bucket"
+  key            = "environments/dev/terraform.tfstate"
+  region         = "us-east-1"
+  dynamodb_table = "viswa-infra"
+ }
+}
+provider "aws" {
+    region = var.aws_region
+    }
+
---
+++ environments/production/main.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source = "../../modules/vpc"
+  vpc_cidr = "15.0.0.0/16"
+  subnet_cidr = "15.0.1.0/24"
+  subnet_az =  "${var.aws_region}a"  # Assuming the region is ap-southeast-1, this will be ap-southeast-1a
+  env = "dev"
+}
+
+module "ec2" {
+   source = "../../modules/ec2"
+   ami_id = "ami-0a7d80731ae1b2435"
+   instance_type = "t2.2xlarge"
+   subnet_id = module.vpc.subnet_id
+   ec2_count = 2
+   env = "dev"
+
+    
+}
+
+module "s3" {
+  source = "../../modules/s3"
+  bucket_name = "viswa-infrabucket"
+  env    = "dev"
+}
